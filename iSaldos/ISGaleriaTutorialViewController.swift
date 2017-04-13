71abcede032afeab0db0a6816c6c0a530f3a5b0f
Felipe <info@icologic.co>
2017-04-13 20:35:29 +0200

    Ajustes_Desarrollo_9

--- iSaldos/ISGaleriaTutorialViewController.swift
+++ iSaldos/ISGaleriaTutorialViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class ISGaleriaTutorialViewController: UIViewController {
+ 
+    //MARK: - IBOutlets
+    @IBOutlet weak var myScrollShowGallery: UIScrollView!
+    @IBOutlet weak var myPageShowGallery: UIPageControl!
+    @IBOutlet weak var myOmitirBTN: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let anchoImagen = self.view.frame.size.width
+        let altoImagen = self.view.frame.size.height
+        
+        for c_imagen in 0..<8{
+            let imagenes = UIImageView(image: UIImage(named: String(format: "FOTO_%d.jpg", c_imagen)))
+            imagenes.frame = CGRect(x: CGFloat(c_imagen) * anchoImagen,
+                                    y: 0,
+                                    width: anchoImagen,
+                                    height: altoImagen)
+            myScrollShowGallery.addSubview(imagenes)
+            myOmitirBTN.isHidden = false
+        }
+        
+        myScrollShowGallery.delegate = self
+        myScrollShowGallery.contentSize = CGSize(width: 7 * anchoImagen, height: altoImagen)
+        myScrollShowGallery.isPagingEnabled = true
+        myPageShowGallery.numberOfPages = 7
+        myPageShowGallery.currentPage = 0
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +49,15 @@ class ISGaleriaTutorialViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension ISGaleriaTutorialViewController : UIScrollViewDelegate{
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = myScrollShowGallery.contentOffset.x / myScrollShowGallery.frame.size.width
+        myPageShowGallery.currentPage = Int(page)
     }
-    */
-
+    
 }
