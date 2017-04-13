6bacd93acb1ae2103a86c06071e33e72fe77ad26
Felipe <info@icologic.co>
2017-04-13 16:45:44 +0200

    Ajustes_Desarrollo_4

---
+++ iSaldos/APIHELPERS.swift
@@ -0,0 +1,9 @@
+//
+//  APIHELPERS.swift
+//  iSaldos
+//
+//  Created by Andres on 13/4/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import Foundation
---
+++ iSaldos/APIUTILS.swift
@@ -0,0 +1,9 @@
+//
+//  APIUTILS.swift
+//  iSaldos
+//
+//  Created by Andres on 13/4/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import Foundation
